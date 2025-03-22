df425798cda5c50cbc4ac023a4fe90cfe839b1bb
Chouaib <chouaib.skt@gmail.com>
2025-03-22 17:37:34 +0100

    feat: implement SpringDataUserRepository to extend JpaRepository and enhance user data access methods

--- services/auth-service/src/main/java/com/academiaconnect/auth/authservice/infrastructure/persistence/SpringDataUserRepository.java
+++ services/auth-service/src/main/java/com/academiaconnect/auth/authservice/infrastructure/persistence/SpringDataUserRepository.java
@@ -1,4 +1,20 @@
 package com.academiaconnect.auth.authservice.infrastructure.persistence;
 
-public interface SpringDataUserRepository {
+import com.academiaconnect.auth.authservice.domain.model.User;
+import com.academiaconnect.auth.authservice.domain.repository.UserRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SpringDataUserRepository extends UserRepository, JpaRepository<User, Long> {
+    @Override
+    Optional<User> findByUsername(String username);
+
+    @Override
+    Optional<User> findByEmail(String email);
+
+    @Override
+    User save(User user);
 }
