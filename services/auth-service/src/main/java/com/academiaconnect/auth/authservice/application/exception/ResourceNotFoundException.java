b55ecd6c8ac483a40b29530318c3f03e43c2e063
Chouaib <chouaib.skt@gmail.com>
2025-03-23 15:34:29 +0100

    feat: implement ResourceNotFoundException with custom message handling

--- services/auth-service/src/main/java/com/academiaconnect/auth/authservice/application/exception/ResourceNotFoundException.java
+++ services/auth-service/src/main/java/com/academiaconnect/auth/authservice/application/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.academiaconnect.auth.authservice.application.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
